58c3627e2fa048912a81ac017a694e89ea2735a4
cyrille <aviateur22@hotmail.fr>
2024-03-06 22:26:03 +0100

    Add missing element in commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProductDashboardComponent } from './components/product-dashboard/product-dashboard.component';
 import { ProductInCommandComponent } from './components/product-in-command/product-in-command.component';
 import { ProductStoreComponent } from './components/product-store/product-store.component';
-import { MainRoundButtonComponent } from './components/main-round-button/main-round-button.component';
-import { SecondaryRoundButtonComponent } from './components/secondary-round-button/secondary-round-button.component';
-import { MainRectangleButtonComponent } from './components/main-rectangle-button/main-rectangle-button.component';
-import { SecondaryRectangleButtonComponent } from './components/secondary-rectangle-button/secondary-rectangle-button.component';
 import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import { RectangleButtonComponent } from './components/rectangle-button/rectangle-button.component';
+import { RoundButtonComponent } from './components/round-button/round-button.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +19,8 @@ import { SearchFilterComponent } from './components/search-filter/search-filter.
     ProductDashboardComponent,
     ProductInCommandComponent,
     ProductStoreComponent,
-    MainRoundButtonComponent,
-    SecondaryRoundButtonComponent,
-    MainRectangleButtonComponent,
-    SecondaryRectangleButtonComponent,
-    SearchFilterComponent
+    RectangleButtonComponent,
+    RoundButtonComponent
   ],
   imports: [
     BrowserModule,
